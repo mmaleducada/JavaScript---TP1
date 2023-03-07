b2bd87e1e0944be3d2d5a61959a73aaa187f288c
Malena de Arriba <dearribamalena@gmail.com>
2023-03-06 23:48:38 -0300

    ejercicio 6 finalizado

---
+++ js/ejercicio6.js
@@ -0,0 +1,8 @@
+let n1 = parseInt(prompt("ingresar el primer numero"));
+let n2 = parseInt(prompt("ingresar el segundo numero"));
+
+if (n1 < n2) {
+    console.log(n2);
+} else {
+    console.log(n1);
+}
