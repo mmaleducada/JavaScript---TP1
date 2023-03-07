d4465a91ab4c50f595321212a844a0f82f3325f8
Malena de Arriba <dearribamalena@gmail.com>
2023-03-07 15:17:47 -0300

    arreglo ejercicio 11

--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,18 +1,15 @@
-let n1 = prompt("Escribe un número");
+let n1 = parseInt(prompt("Escribe un número"));
 
 if (n1 % 2 === 0 || n1 % 3 === 0 || n1 % 5 === 0 || n1 % 7 === 0) {
-if (n1 % 2 === 0) {
-document.write("Es divisible por 2. ");
-}
-if (n1 % 3 === 0) {
-document.write("Es divisible por 3. ");
-}
-if (n1 % 5 === 0) {
-document.write("Es divisible por 5. ");
-}
-if (n1 % 7 === 0) {
-document.write("Es divisible por 7. ");
-}
+    if (n1 % 2 === 0) {
+        document.write("Es divisible por 2");
+    } if (n1 % 3 === 0) {
+        document.write("Es divisible por 3");
+    } if (n1 % 5 === 0) {
+        document.write("Es divisible por 5");
+    } if (n1 % 7 === 0) {
+        document.write("Es divisible por 7");
+    }
 } else {
-document.write("No es divisible ni por 2, ni por 3, ni por 5, ni por 7");
+    document.write("No es divisible ni por 2, ni por 3, ni por 5, ni por 7");
 }
