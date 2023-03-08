0cd755fecb137390d0eaf44cccb0882a9bd34758
Malena de Arriba <dearribamalena@gmail.com>
2023-03-07 22:20:15 -0300

    ejercicio 9 terminado con la explicacion de emilse

---
+++ js/ejercicio9.js
@@ -0,0 +1,19 @@
+const frase = prompt("Ingrese una frase");
+
+console.log(frase.length);
+
+if (frase.charAt(0) === "a") || (frase.charAt(0) === "e") || (frase.charAt(0) === "i") || (frase.charAt(0) === "o") || (frase.charAt(0) === "u") {
+    document.write(frase.charAt(0));
+}
+
+if (frase.charAt(1) === "a") || (frase.charAt(1) === "e") || (frase.charAt(1) === "i") || (frase.charAt(1) === "o") || (frase.charAt(1) === "u") {
+    document.write(frase.charAt(0));
+}
+
+if (frase.charAt(2) === "a") || (frase.charAt(2) === "e") || (frase.charAt(2) === "i") || (frase.charAt(2) === "o") || (frase.charAt(2) === "u") {
+    document.write(frase.charAt(0));
+}
+
+if (frase.charAt(3) === "a") || (frase.charAt(3) === "e") || (frase.charAt(3) === "i") || (frase.charAt(3) === "o") || (frase.charAt(3) === "u") {
+    document.write(frase.charAt(0));
+}
